884f9c0cdddd1bc23c67fa992f13ed989f79f42e
laravue <laravue@psp.ru>
2025-01-28 15:56:00 +0500

    Fix: prepare for production

--- frontend/src/layouts/ui/LayoutUiFonts.vue
+++ frontend/src/layouts/ui/LayoutUiFonts.vue
@@ -18,15 +18,15 @@
           <div class="title mr-10">
             <span class="h1 fw-bold">H1:</span>
             <span class="h2 fw-bold">H2:</span>
-            <span class="h3 fw-bold">H3 bold:</span>
-            <span class="h3 fw-semibold">H3 semibold:</span>
-            <span class="h3">H3 regular:</span>
-            <span class="h4 fw-bold">H4 bold:</span>
-            <span class="h4 fw-semibold">H4 semibold:</span>
-            <span class="h4">H4 regular:</span>
-            <span class="h5 fw-bold">H5 bold:</span>
-            <span class="h5 fw-semibold">H5 semibold:</span>
-            <span class="h5">H5 regular:</span>
+            <span class="h3 fw-bold">H3 </br> bold:</span>
+            <span class="h3 fw-semibold">H3 </br> semibold:</span>
+            <span class="h3">H3 </br> regular:</span>
+            <span class="h4 fw-bold">H4 </br> bold:</span>
+            <span class="h4 fw-semibold">H4 </br> semibold:</span>
+            <span class="h4">H4 </br> regular:</span>
+            <span class="h5 fw-bold">H5 </br> bold:</span>
+            <span class="h5 fw-semibold">H5 </br> semibold:</span>
+            <span class="h5">H5 </br> regular:</span>
           </div>
           <div class="text">
             <span class="h1 fw-bold">Система предназначена</span>
